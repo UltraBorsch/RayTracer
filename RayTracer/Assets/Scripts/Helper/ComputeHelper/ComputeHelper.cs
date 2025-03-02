bfce4546368048470ec7207abfd7cd6140e234b7
Joshua-M1 <jsmorency@hotmail.com>
2025-03-02 01:48:36 -0500

    reorganized assets. made a folder for ComputeHelper, IStructable, and any other shader helper scripts i end up making. added fancy method summaries to all the methods in both those scripts. some more minor cleanup

--- RayTracer/Assets/Scripts/Helper/ComputeHelper/ComputeHelper.cs
+++ RayTracer/Assets/Scripts/Helper/ComputeHelper/ComputeHelper.cs
@@ -6,17 +6,19 @@ using System.Threading;
 using UnityEngine;
 
 public static class ComputeHelper {
-    private static List<ComputeBuffer> createdBuffers = new();
+    private static readonly List<ComputeBuffer> createdBuffers = new();
 
+    //static constructor. mostly just subscribes to the application quitting event.
     static ComputeHelper() {
         Application.quitting += OnQuit;
     }
 
+    //on quit, release all the buffers.
     private static void OnQuit() {
         Release(createdBuffers.ToArray());
     }
 
-    //run a kernel of a shader, calculates optimal dispatch sizes
+    /// <summary> Dispatch a kernel of a shader. Also calculates optimal dispatch sizes. </summary>
     //  thread sizes/dimension in kernel is how many threads the gpu will run during the job (usually multiple of 32 or 64)
     //  group size in dispatch is how many times per dimension we want to call the shader.
     //  optimal to call shader as few times as possible, so x/y/z iterations should be how many things in that dimension i need to do
@@ -30,7 +32,7 @@ public static class ComputeHelper {
         shader.Dispatch(shader.FindKernel(kernel), xGroups, yGroups, zGroups);
     }
 
-    //handles all simple param passing (i.e. not textures and buffers, since those require kernel indices and whatnot)
+    /// <summary> Handles all simple parameter passing to the given ComputeShader. </summary>
     public static void SetParam<T>(ComputeShader shader, T value, string valueName) {
         switch (value) {
             case int i: shader.SetInt(valueName, i); break;
@@ -49,20 +51,20 @@ public static class ComputeHelper {
         }
     }
 
-    //get thread group sizes for each dimension of a kernel
+    /// <summary> Get thread group sizes for each dimension of a kernel.</summary>
     public static Vector3Int GetThreadGroupSizes(ComputeShader shader, string kernel) {
         shader.GetKernelThreadGroupSizes(shader.FindKernel(kernel), out uint x, out uint y, out uint z);
         return new((int)x, (int)y, (int)z);
     }
 
-    //creates and returns a buffer from basic data
+    /// <summary> Creates and returns a buffer from basic data. </summary>
     public static ComputeBuffer CreateAndSetBuffer<T>(ComputeShader shader, string kernel, string BufferName, int size) {
         ComputeBuffer buffer = CreateBuffer<T>(size);
         shader.SetBuffer(shader.FindKernel(kernel), BufferName, buffer);
         return buffer;
     }
 
-    //creates and returns a buffer from data. will also fill with values
+    /// <summary> Creates and returns a buffer from data. will also fill with the buffer with provided values. </summary>
     public static ComputeBuffer CreateAndSetBuffer<T>(ComputeShader shader, string kernel, string BufferName, T[] arr) where T : struct {
         ComputeBuffer buffer = CreateBuffer<T>(arr.Length);
         buffer.SetData(arr);
@@ -70,6 +72,7 @@ public static class ComputeHelper {
         return buffer;
     }
 
+    /// <summary> Creates buffer from an array of IStructable objects. </summary>
     public static ComputeBuffer CreateAndSetBuffer<T>(ComputeShader shader, string kernel, string BufferName, IStructable<T>[] arr) where T : struct {
         int count = arr.Length;
 
@@ -83,17 +86,20 @@ public static class ComputeHelper {
         return buffer;
     }
 
+    /// <summary> Create a buffer of a given size of type T. </summary>
     public static ComputeBuffer CreateBuffer<T>(int size) {
         ComputeBuffer buffer = new(size, System.Runtime.InteropServices.Marshal.SizeOf(typeof(T)));
         createdBuffers.Add(buffer);
         return buffer;
     }
 
+    /// <summary> Fills provided array with data contained within the buffer. Then released the buffer. </summary>
     public static void GetDataAndRelease<T>(ComputeBuffer buffer, T[] arr) {
         buffer.GetData(arr);
         Release(buffer);
     }
 
+    /// <summary> Creates an array and fills it with the data contained within the provided Buffer. Then releases the buffer. </summary>
     public static T[] GetDataAndRelease<T>(ComputeBuffer buffer) {
         T[] arr = new T[buffer.count];
         buffer.GetData(arr);
@@ -101,7 +107,7 @@ public static class ComputeHelper {
         return arr;
     }
 
-    /// Releases supplied buffer/s if not null
+    /// <summary> Releases supplied buffer(s) if not null </summary>
     public static void Release(params ComputeBuffer[] buffers) {
         for (int i = 0; i < buffers.Length; i++) {
             buffers[i]?.Release();
@@ -109,7 +115,7 @@ public static class ComputeHelper {
         }
     }
 
-    /// Copy the contents of one render texture into another. Assumes textures are the same size.
+    /// <summary> Copy the contents of one render texture into another. Assumes textures are the same size. </summary>
     public static void CopyRenderTexture(Texture source, RenderTexture target) {
         Graphics.Blit(source, target);
     }
